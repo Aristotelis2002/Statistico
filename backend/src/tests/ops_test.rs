fe289cff6aa19848a62b3f41197a276771143b58
Aristotelis <arispapanis@abv.bg>
2022-09-02 19:58:24 +0300

    object unit tests

--- backend/src/tests/ops_test.rs
+++ backend/src/tests/ops_test.rs
@@ -1,8 +1,7 @@
-
 #[cfg(test)]
 mod tests {
-    use diesel::RunQueryDsl;
     use diesel::sql_query;
+    use diesel::RunQueryDsl;
     use serial_test::serial;
 
     use crate::api::statistic_controller::get_all_statistics;
@@ -12,7 +11,7 @@ mod tests {
     use crate::ops::statistic_ops::*;
     use crate::ops::user_ops::*;
     use diesel::sql_types::Integer;
-    
+
     #[test]
     #[serial]
     fn create_show_del_user() {
@@ -21,26 +20,57 @@ mod tests {
         let user = show_by_user_id(res.unwrap()).unwrap();
         delete_user(user.id); //if it doesnt panic, it works
     }
-   #[test]
-   #[serial]
-    fn stat_test(){
+    #[test]
+    #[serial]
+    fn statistic_ops() {
         let connection = establish_connection();
-        let last_stat:Vec<Statistic>  = sql_query(format!
-            ("SELECT * FROM statistics ORDER BY id DESC LIMIT 1;"))
-            .load(&connection)
-            .expect("SQL query error");
+        let last_stat: Vec<Statistic> = sql_query(format!(
+            "SELECT * FROM statistics ORDER BY id DESC LIMIT 1;"
+        ))
+        .load(&connection)
+        .expect("SQL query error");
         let last_id = last_stat[0].id;
-        println!("STAT ID{}", last_id);
-        let test2: NewStatistic = NewStatistic { 
+        let test2: NewStatistic = NewStatistic {
             name: ("kitchen"),
-            user_id: (15) 
+            user_id: (15),
         };
-        create_statistic(test2); 
+        create_statistic(test2);
         let mut res = show_statistic_by_id(last_id + 1);
         res.name = String::from("shoes");
         update_statistic(res);
         let res2 = show_statistic_by_id(last_id + 1);
         assert_eq!(res2.name, "shoes");
         delete_statistic(last_id + 1);
+    }
+    #[test]
+    #[serial]
+    fn object_ops() {
+        let connection = establish_connection();
+        let last_obj: Vec<Object> =
+            sql_query(format!("SELECT * FROM objects ORDER BY id DESC LIMIT 1;"))
+                .load(&connection)
+                .expect("SQL query error");
+        let last_id = last_obj[0].id;
+        let test3: NewObject = NewObject {
+            name: "red book",
+            statistic_id: 15,
+            counter: 4,
+        };
+        create_object(test3);
+        let res = show_object_by_id(last_id + 1);
+        assert_eq!(res.name, "red book");
+        // delete_object(2);
+        update_counter(last_id + 1, 24);
+        let res = show_object_by_id(last_id + 1);
+        assert_eq!(res.counter, 24);
+        update_object(Object {
+            id: last_id + 1,
+            name: String::from("yellow book"),
+            statistic_id: 15,
+            counter: 54,
+        });
+        let res = show_object_by_id(last_id + 1);
+        assert_eq!(res.name, "yellow book");
+        assert_eq!(delete_object(last_id + 1), true);
     }
 }
